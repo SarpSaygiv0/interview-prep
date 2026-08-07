6fdc55db3c34d6aa7cef8427aeeda784bbe05675
Sarp Saygi <sarpalisaygi@gmail.com>
2026-08-07 17:44:58 +0300

    removed comments from the substr solution

--- src/main/java/com/medium/longesubstrworepeatingchars/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/medium/longesubstrworepeatingchars/LongestSubstringWithoutRepeatingCharacters.java
@@ -6,20 +6,6 @@ import java.util.Set;
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String str) {
-
-        /*
-
-          longest = 3
-          left = right
-          l             r
-          0 1 2 3 4 5 6 7
-         "a b c a b c b b"
-
-          0 1 2 3 4 5 6 7 8 9 10 11 12 13
-         "a b c a b c b d e f g  h  i  b"
-
-         */
-
         Set<Character> tracker = new HashSet<>();
         int left = 0;
         int longestLength = 0;
